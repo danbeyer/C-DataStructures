85c4f9df136528df3640270061cafccd9cb3bfdd
danbeyer <dan.beyer@gmail.com>
2016-09-20 17:43:22 -0700

    Add files via upload

---
+++ Assign4/structs.h
@@ -0,0 +1,12 @@
+/* You can modify the structure to store whatever you'd like in your BST
+* CS261- Assignment 4 - Implementation of the binary search tree data structure.
+* Name: Daniel Beyer
+ * Date: 07/23/16
+ * Solution description: Implementing a binary search tree that can store any arbitrary
+ * structs in its nodes.
+ */
+
+struct data {
+	int number;
+	char *name;
+};
